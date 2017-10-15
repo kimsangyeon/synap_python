ec7fe47005f472d225f939b8f9a1398a3fdb36a3
yeon <dho113@naver.com>
2017-10-15 21:30:30 +0900

    hackerrank/BasicDataTypes/Finding the percentage - 학생 선택하여 평균 값 구하기

---
+++ sangyeon/hackerrank/BasicDataTypes/percentage.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    scoreArr = student_marks[query_name]
+    print('%.2f' % float(sum(scoreArr, 0.0) / len(scoreArr)))
