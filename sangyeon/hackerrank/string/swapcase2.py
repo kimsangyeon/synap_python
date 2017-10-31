e0938734121eb514d83d0ffcaa34e7b54d08af4d
yeon <dho113@naver.com>
2017-10-31 22:29:16 +0900

    hackerrank/string/swapcase - ascii 코드로 소문자 대문자 검사후 lower, upper

--- sangyeon/hackerrank/string/swapcase2.py
+++ sangyeon/hackerrank/string/swapcase2.py
@@ -1,11 +1,23 @@
+LOWER_CASE_A = 97
+LOWER_CASE_Z = 122
+UPPER_CASE_A = 65
+UPPER_CASE_Z = 90
+
 def swap_case(s):
-    swap = ''
-    for a in s:
-        if a.isalpha():
-            if 65 <= ord(a) and ord(a) <= 90:
-                swap += a.lower()
-            elif 97<= ord(a) and ord(a) <= 122:
-                swap += a.upper()
-        else:
-            swap += a
-    return swap
+    sList = []
+    for c in s:
+        sList.append(swap(c))
+    
+    return "".join(sList)
+
+def swap(c):
+    if UPPER_CASE_A <= ord(c) and ord(c) <= UPPER_CASE_Z:
+        return c.lower()
+    elif LOWER_CASE_A <= ord(c) and ord(c) <= LOWER_CASE_Z:
+        return c.upper()
+    return c
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
