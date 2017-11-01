64bf0f482fb83cbea0188118817585163e525df3
yeon <dho113@naver.com>
2017-11-02 00:35:10 +0900

    hackerrank/string/find a string - sub_string 만큼에 길이로 탐색 코드

--- sangyeon/hackerrank/string/findString.py
+++ sangyeon/hackerrank/string/findString.py
@@ -1,5 +1,8 @@
 def count_substring(string, sub_string):
     count = 0
+    for i in range(0, len(string)):
+        if string[i:len(sub_string) + i] == sub_string:
+            count += 1
     return count
 
 if __name__ == '__main__':
