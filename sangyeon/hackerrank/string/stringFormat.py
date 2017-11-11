dc1c2683145830051a42c6a90bf10f1da8108c02
yeon <dho113@naver.com>
2017-11-11 12:28:35 +0900

    hackerrank/string/string format - 반복문 돌며 replace_formatted에서 적절한 문자열로 변환 - rjust로 정렬후 출력

--- sangyeon/hackerrank/string/stringFormat.py
+++ sangyeon/hackerrank/string/stringFormat.py
@@ -1,5 +1,16 @@
 def print_formatted(number):
-	print
+    width = len(replace_formatted(bin(number)))
+    for i in range(0, number):
+        print(str(i + 1).rjust(width, ' ') + ' ' + replace_formatted(str(oct(i + 1))).rjust(width, ' ') + ' ' + replace_formatted(str(hex(i + 1))).rjust(width, ' ') + ' ' + replace_formatted(str(bin(i + 1))).rjust(width, ' '))
+        
+def replace_formatted(n):
+    if n.find('0o') == 0:
+        n = n.replace('0o', '')
+    elif n.find('0x') == 0:
+        n = n.replace('0x', '').upper()
+    elif n.find('0b') == 0:
+        n = n.replace('0b', '')
+    return n
 
 if __name__ == '__main__':
     n = int(input())
