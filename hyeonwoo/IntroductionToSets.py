2748c30eb8d16b7a164323e4ad30adeceb68dc29
macroness <32091521+macroness@users.noreply.github.com>
2017-12-02 23:32:10 +0900

    [hyeonwoo-171202] Introduction to Sets

---
+++ hyeonwoo/IntroductionToSets.py
@@ -0,0 +1,11 @@
+
+def average(array):
+    array = list(set(array))
+    return sum(array, 0.0)/len(array)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
